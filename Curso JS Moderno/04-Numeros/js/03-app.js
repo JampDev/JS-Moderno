b3d52f65e7f9a03c1778f976d5a028a4b3402a0b
Jonathan <jamorochop@uce.edu.ec>
2023-01-12 21:26:34 -0500

    JM: Seccion numeros

---
+++ Curso JS Moderno/04-Numeros/js/03-app.js
@@ -0,0 +1,26 @@
+let resultado;
+
+resultado = Math.PI;
+
+resultado = Math.round(1.976);
+
+//Redondea arriba
+resultado = Math.ceil(2.1);
+//Redonde abajo
+resultado = Math.floor(2.1);
+
+resultado = Math.sqrt(25);
+
+reultado = Math.abs(-55);
+
+resultado = Math.pow(8,3);
+
+resultado = Math.min(2, 4, 0, 10);
+
+resultado = Math.max(2, 4, 0, 10);
+
+resultado = Math.random();
+
+resultado = Math.floor(Math.random() * 30);
+
+console.log(resultado);
